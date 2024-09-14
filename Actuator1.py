98d23d154167ca79979613ec51fbf3c47cd1a31b
Dani Mirkin <95719644+Dan1k99@users.noreply.github.com>
2024-09-14 17:35:05 +0300

    Update Actuator1.py

---
+++ Actuator1.py
@@ -0,0 +1,72 @@
+import rticonnextdds_connector as rti
+from os import path as osPath
+from time import sleep
+
+# Set the filepath to the directory of this script
+filepath = osPath.dirname(osPath.realpath(__file__))
+
+# Configure connection
+connector = rti.Connector("MyParticipantLibrary::Actuator1", filepath + "/DDS.xml")
+
+# Configure all Inputs (Subscribers of Actuator)
+input_DDS_button = connector.getInput("Actuator1::Actuator_button")
+input_DDS_temp1 = connector.getInput("Actuator1::Actuator_Tempsensor1")
+
+# Configure all Outputs (Publishers of Actuator)
+output_DDS_actuator_status = connector.getOutput("actuator_publisher1::status_writer")
+output_DDS_temp_extreme = connector.getOutput("actuator_publisher2::temp_extreme_writer")
+
+# Initialize parameters
+button_state = 1  # Default to start
+actuator_status_list = ["Working", "Degraded", "Stopped"]
+status_index = 0  # Will decide the actuator's status
+temp1_message = 0
+temp1_status = False
+
+def get_temp_status(num_samples):
+    """This function returns True if the temp sensor is functioning, False otherwise."""
+    return num_samples > 0  # If no samples, it means the temp sensor is not running
+
+while True:
+    sleep(1)  # Sleep for 1 second
+
+    # Take the data from the button input
+    input_DDS_button.take()
+    num_samples_button = input_DDS_button.samples.getLength()
+    for i in range(num_samples_button):
+        if input_DDS_button.infos.isValid(i):
+            button_state = input_DDS_button.samples.getNumber(i, 'Button')
+
+    # Update the actuator status based on the button state
+    if button_state == 0:  # Received stop from StartStop
+        if status_index in [0, 1]:
+            status_index = 2
+    elif button_state == 1 and status_index == 2:  # Received start from StartStop
+        status_index = 0
+
+    if status_index != 2:  # If actuator is working, connect to all sensors
+        input_DDS_temp1.take()
+        num_samples_temp1 = input_DDS_temp1.samples.getLength()
+
+        # Receive all temperature information
+        for i in range(num_samples_temp1):
+            if input_DDS_temp1.infos.isValid(i):
+                temp1_message = input_DDS_temp1.samples.getNumber(i, "Temp")
+
+        temp1_status = get_temp_status(num_samples_temp1)  # Check which temps are running
+
+        # Checking for extreme temperatures
+        if temp1_status:
+            if temp1_message < 18 or temp1_message > 45:
+                output_DDS_temp_extreme.instance.setNumber("Tempsensor1_extreme", temp1_message)
+                output_DDS_temp_extreme.write()  # Send the extreme temps to the dashboard
+                status_index = 1
+            else:
+                status_index = 0
+
+    # Print the current actuator status
+    print(f'Actuator status: {actuator_status_list[status_index]}')
+
+    # Publish the current actuator status
+    output_DDS_actuator_status.instance.setString("ActuatorStatus", actuator_status_list[status_index])
+    output_DDS_actuator_status.write()
